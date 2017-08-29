db7b4e970b3d8f83e6df4a868da9e81e0c6befbc
Medson <medson.oliveira.jr@gmail.com>
2017-08-29 01:11:57 -0300

    Adiciona correções

--- pdf.js
+++ pdf.js
@@ -1,21 +1,19 @@
 const pdf = require('html-pdf');
 
 module.exports = (req, res) => {
-    var html = req.body.content;
-    var options = {format: 'Letter', orientation: 'portrait', border: {
-        "top": "2cm",
-        "right": "1.5cm",
-        "bottom": "2cm",
-        "left": "1.5cm"
-    }};
+  const html = req.body.content;
+  const options = {format: 'Letter', orientation: 'portrait', border: {
+      "top": "2cm",
+      "right": "1.5cm",
+      "bottom": "2cm",
+      "left": "1.5cm"
+  }};
 
-    pdf.create(html, options).toFile('PDF/prova.pdf', function(err, response){
-        res.status(200).json({
-            data: {
-                name: 'PDF Generated!'
-            }
-        });
-    });
-
-
+  pdf.create(html, options).toFile('PDF/prova.pdf', function(err, response){
+      res.status(200).json({
+          data: {
+              name: 'PDF Generated!'
+          }
+      });
+  });
 }
